79c7ab176bc138f4234c420b53133c4971966fff
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:48:41 +0100

    Creamos el ejercicio 8 de repaso. Calcularemos la serie de fibonacci.

---
+++ repaso8/Repaso8.java
@@ -0,0 +1,21 @@
+/*
+ * Crea un hilo que realice el cálculo de los primeros N números de la sucesión de Fibonacci.
+ * El parámetro N será indicado cuando se llame al constructor de la clase Thread correspondiente.
+ */
+package repaso8;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
