0bbcdb71fbbc89000c0cd6dc99c53b9039482883
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:59:06 +0100

    Creamos la clase Hilo.

---
+++ repaso8/Hilo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repaso8;
+
+/**
+ *
+ * @author findag
+ */
+public class Hilo extends Thread
+{
+    int n;
+    public Hilo(int n)
+    {
+        this.n = n;
+    }
+    
+    public void fibo()
+    {
+        
+    }
+}
